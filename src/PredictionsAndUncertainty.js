f7bc8cd9104ea091cc8365fc165875d3e4f63f4b
Vaidehi Srinivas <vaidehi.8913@gmail.com>
2025-05-08 18:29:52 -0500

    contact link

--- src/PredictionsAndUncertainty.js
+++ src/PredictionsAndUncertainty.js
@@ -483,6 +483,8 @@ class PredictionsAndUncertainty extends Component {
                     <li><strong>Poster acceptance notification:</strong> June 1, 2025 </li>
                     </ul>
 
+                    <p>Please feel free to reach out to <a href="mailto:vaidehi@u.northwestern.edu">Vaidehi</a> with any questions regarding the poster submission.</p>
+
                     <p>The schedule of invited talks is coming soon!</p>
                 </div>
                 </section>
